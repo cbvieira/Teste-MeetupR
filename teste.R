599dbaf070e5fb46ed587d3729fc355e1f72a47d
Camila Batista Vieira <camila.bt.vieira@gmail.com>
2021-09-11 23:53:30 -0300

    add clonar rtudio

--- teste.R
+++ teste.R
@@ -1,3 +1,5 @@
+library("usethis")
+
 usethis::use_git()
 usethis::use_github()
 usethis::use_readme_md()
@@ -6,5 +8,13 @@ usethis::use_readme_md()
 usethis::pr_init(branch = "teste")
 usethis::pr_push()
 
-camila - batista - vieira - 30 anos
-teste
+# camila - batista - vieira - 30 anos
+# teste
+
+
+usethis::create_from_github("R-Ladies-Sao-Paulo/RLadies-Brasil",
+                            destdir = "C:/Users/camil/GitHub/CAMILA/Teste-MeetupR",
+                            fork = TRUE)
+
+
+2getwd()
